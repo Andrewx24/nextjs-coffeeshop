c873480c2e01ddd86361a3728c60195ebe77613a
Andrew Aliaj <andyaliaj@gmail.com>
2024-06-08 08:02:51 -0400

    new updates

--- ui/componets/Navbar.tsx
+++ ui/componets/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from  '@/assets/logo/logo.png'
 
 const Navbar = () => {
   return (
     <div className="w-full bg-gray-800">
       <nav className="flex justify-center items-center h-16">
+      <div className="flex justify-start  items-start h-16">
+        <Image src= {logo} width={100} height={100}  alt='logo'/>
+      </div>
         <ul className="flex items-center text-white space-x-6">
           <li>
             <Link href="/">Home</Link>
